f4a1db192a6b898287090f6f650848ecd95f06e4
arivusurya <arivusurya.3@gmail.com>
2023-04-03 23:33:05 +0530

    validate the email

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ const outputsalary = document.querySelector('.salary-output')
 salary.addEventListener("input",()=>{
     outputsalary.textContent=salary.value
 })
+
+const email = document.querySelector("#email")
+const email_error = document.querySelector("#email-error")
+
+email.addEventListener('input',()=>{
+    const emailregex= RegExp("^[A-Za-z0-9]+(.[a-zA-Z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,}(.[A-Za-z]{2,})?$")
+    if(emailregex.test(email.value))
+        email_error.textContent=""
+    else
+        email_error.textContent="Email is invalid"
+ })
